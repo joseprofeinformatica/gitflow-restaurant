5708e1c2b95027bf1df27b6d3849e405aedb2852
josearodriguez <jose.a.torres95@gmail.com>
2025-10-07 23:10:32 +0200

    Nueva funcionalidad

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,5 @@ if (acceso) {
 export function procesarReserva(cliente: string, total: number): string {
   console.log(`Procesando reserva para ${cliente}.`);
   console.log(`Total a pagar: ${total} €`);
-
-  let puntosGanados = Math.floor(total / 10);
-  console.log(`🎁 El cliente ${cliente} ha ganado ${puntosGanados} puntos.`);
-  
-  return "Reserva completada correctamente con puntos de recompensa.";
+  return "Reserva completada correctamente.";
 }
